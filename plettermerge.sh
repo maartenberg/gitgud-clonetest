b21dbe3ca0aa3ca69fa9914203507df1a7076539
Maarten van den Berg <maartenberg1@gmail.com>
2016-11-01 09:26:09 +0100

    Fix plettermerge zodat niet alles wordt weggegooid

--- plettermerge.sh
+++ plettermerge.sh
@@ -1,7 +1,7 @@
 # Zeg maar dag tegen je merge conflict!
 
 git pull $1
-cat README.md | sed -r '/(<<<<|====|>>>>).*/d' > README.md
+sed -i.bak -r '/(<<<<|====|>>>>).*/d' README.md
 git add README.md
 git commit -m "Plettermerge $1"
 git push
